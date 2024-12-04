e87126b1ef3662db34dfc188d8e855cb70813980
Winsice Ng <wn4759@princeton.edu>
2024-12-04 03:04:13 -0500

    Add css to left side of Customize page

--- src/pages/Customize.js
+++ src/pages/Customize.js
@@ -4,11 +4,11 @@ import NavBar from "../components/NavBar";
 import FormText from "../components/FormText";
 import ColorSelect from "../components/ColorSelect";
 import DropDown from "../components/DropDown";
-import "./Customize.css";
 // import EmojiSelector from "../components/EmojiSelector";
 import LogoUpload from "../components/LogoUpload";
 import ButtonSelector from "../components/ButtonSelector";
 import Preview from "../components/Preview";
+import "./Customize.css";
 
 // new commit comment
 
@@ -40,21 +40,31 @@ const Customize = ({ onLogout }) => {
         {/* Pass selectedTab */}
       </div>
       <div className="right">
-        <div className="container-col">
-          <FormText title="Game Title" titleid="gameTitle"></FormText>
-          <LogoUpload></LogoUpload>
-          <DropDown></DropDown>
-          <FormText title="Share Message" titleid="shareMessage"></FormText>
+        <div className="container-col input">
+          <div className="divider"><FormText className="divider" title="Game Title" titleid="gameTitle"></FormText></div>
+          <div className="divider top-padding"><LogoUpload></LogoUpload></div>
+          <div className="divider top-padding"><DropDown></DropDown></div>
+          <div className="divider top-padding"><FormText title="Share Message" titleid="shareMessage"></FormText></div>
           {/* <EmojiSelector></EmojiSelector> */}
-          <ButtonSelector></ButtonSelector>
+          <div className="dividier top-padding">Streak Emoji</div>
+          <div className="divider top-padding">Button Style 
+            <div className="top-padding"><ButtonSelector></ButtonSelector></div>
+          </div>
         </div>
 
-        <div className="container-row">
-          <Preview></Preview>
-          <ColorSelect label="Primary"></ColorSelect>
-          <ColorSelect label="Secondary"></ColorSelect>
-          <ColorSelect label="Primary"></ColorSelect>
-          <ColorSelect label="Secondary"></ColorSelect>
+        <div className="container-col preview">
+          <div>Preview</div><div className="top-padding"><Preview></Preview></div>
+          <div className="container-row">
+            <div className="container-row">
+              Font
+              <ColorSelect label="Primary"></ColorSelect>
+              <ColorSelect label="Secondary"></ColorSelect>
+            </div>
+            <div className="container-row">
+              <ColorSelect label="Primary"></ColorSelect>
+              <ColorSelect label="Secondary"></ColorSelect>
+            </div>
+          </div>
         </div>
       </div>
     </div>
